38d5705ec496d069224d3079e30fcc5c9d947798
Ryan Maxwell Rasi <theryanrasi@gmail.com>
2021-01-28 16:26:21 +0000

    Splits the input into tokens and keywords

--- token/token.go
+++ token/token.go
@@ -1,23 +1,30 @@
 package token
 
-type TokenType string //Allows for many different values as TokenTypes
+type TokenType string
 
-type Token struct {
-	Type    TokenType
-	Literal string //String means that it is easy to debug
-}
+//Strings are easy to debug
 
-//All TokenTypes are constants
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
+
 	// Identifiers + literals
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"   // 1343456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ     = "=="
+	NOT_EQ = "!="
 
 	// Delimiters
 	COMMA     = ","
@@ -31,4 +38,33 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	//Returns the correct TokenType for special statements
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	//Checks the keywords to see whether the identifier is a keyword
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
