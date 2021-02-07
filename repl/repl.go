da9562bde9248a36a54271890c413056a43c5767
Ryan Maxwell Rasi <theryanrasi@gmail.com>
2021-02-07 11:57:49 +0000

    repl evaluates each line input

--- repl/repl.go
+++ repl/repl.go
@@ -1,18 +1,22 @@
 package repl
 
 import (
-	"../lexer"
-	"../parser"
 	"bufio"
 	"fmt"
 	"io"
+
+	"../evaluator"
+	"../lexer"
+	"../object"
+	"../parser"
 )
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	//For every line
+	env := object.NewEnvironment()
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -30,14 +34,17 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
-	io.WriteString(out, "An error has occurred!\n")
-	io.WriteString(out, " parser errors:\n")
+	io.WriteString(out, "An error has occured!\n")
+	io.WriteString(out, " - Parser errors:\n")
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
