a00e7c64cd75882d732932553adf00f3d33527f0
Ryan Maxwell Rasi <theryanrasi@gmail.com>
2021-02-07 21:25:16 +0000

    REPL supports user upload of custom files

--- repl/repl.go
+++ repl/repl.go
@@ -1,43 +1,84 @@
 package repl
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-
 	"../evaluator"
 	"../lexer"
 	"../object"
 	"../parser"
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 const PROMPT = ">> "
 
+var fileLocation = "src/"
+
+var input_files = os.Args[1:]
+
 func Start(in io.Reader, out io.Writer) {
+
+	if len(input_files) < 1 {
+		fmt.Println("Not detected any input files.")
+	} else {
+		fmt.Println("File_name is: ", input_files[0], "\n")
+		fileLocation = fileLocation + input_files[0]
+	}
+
 	scanner := bufio.NewScanner(in)
 	env := object.NewEnvironment()
 
-	for {
-		fmt.Printf(PROMPT)
-		scanned := scanner.Scan()
-		if !scanned {
-			return
+	if fileLocation != "src/" {
+		file, err := os.Open(fileLocation)
+		//fmt.Println("MapReduce - Reading from Data Source")
+		if err != nil { //If there is an error then log the error
+			log.Fatal(err)
 		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file) //For every file, increment the base lines variable
+		for scanner.Scan() {
+			line := scanner.Text()
+			l := lexer.New(line)
+			p := parser.New(l)
 
-		line := scanner.Text()
-		l := lexer.New(line)
-		p := parser.New(l)
+			program := p.ParseProgram()
+			if len(p.Errors()) != 0 {
+				printParserErrors(out, p.Errors())
+				continue
+			}
 
-		program := p.ParseProgram()
-		if len(p.Errors()) != 0 {
-			printParserErrors(out, p.Errors())
-			continue
+			evaluated := evaluator.Eval(program, env)
+			if evaluated != nil {
+				io.WriteString(out, evaluated.Inspect())
+				io.WriteString(out, "\n")
+			}
 		}
+	} else {
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+		for {
+			fmt.Printf(PROMPT)
+			scanned := scanner.Scan()
+			if !scanned {
+				return
+			}
+
+			line := scanner.Text()
+			l := lexer.New(line)
+			p := parser.New(l)
+
+			program := p.ParseProgram()
+			if len(p.Errors()) != 0 {
+				printParserErrors(out, p.Errors())
+				continue
+			}
+
+			evaluated := evaluator.Eval(program, env)
+			if evaluated != nil {
+				io.WriteString(out, evaluated.Inspect())
+				io.WriteString(out, "\n")
+			}
 		}
 	}
 }
