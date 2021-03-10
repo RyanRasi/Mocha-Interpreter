8121d4b3e58cbd2b632df9abfe2e43441aa97ca5
Ryan Maxwell Rasi <theryanrasi@gmail.com>
2021-03-10 21:48:49 +0000

    Random int function added

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -4,8 +4,11 @@ import (
 	"../object"
 	"fmt"
 	//"sort"
+	"math/rand"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 )
 
 var builtins = map[string]*object.Builtin{
@@ -50,6 +53,31 @@ var builtins = map[string]*object.Builtin{
 					args[0].Type())
 			}
 
+		},
+	},
+	//Random Library
+	"random": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2",
+					len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.Integer:
+				switch arg1 := args[1].(type) {
+				case *object.Integer:
+					rand.Seed(time.Now().UnixNano())
+					min := arg.Value
+					max := arg1.Value
+					return &object.Integer{Value: int64(rand.Int63n(max-min+1) + min)}
+				default:
+					return newError("Second argument must be an INTEGER, got %s",
+						args[1].Type())
+				}
+			default:
+				return newError("First argument must be an INTEGER, got %s",
+					args[0].Type())
+			}
 		},
 	},
 	//Prints first element of array
@@ -197,6 +225,54 @@ var builtins = map[string]*object.Builtin{
 					len(args))
 			}
 			return &object.String{Value: ("For additional help please access the gitHub repository at https://github.com/ryanrasi/mocha-interpreter")}
+		},
+	},
+	"split": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2",
+					len(args))
+			} else {
+				if args[0].Type() != object.STRING_OBJ {
+					return newError("argument to `split` must be STRING, got %s",
+						args[0].Type())
+				} else if args[1].Type() != object.STRING_OBJ {
+					return newError("argument to `split` must be STRING, got %s",
+						args[1].Type())
+				} else {
+					var stringToSplit = ""
+					var splitter = ""
+					switch arg := args[0].(type) {
+					case *object.String:
+						stringToSplit = arg.Value
+						print(stringToSplit)
+						//print(arg.Value)
+					default:
+						return newError("arguments to must be STRING, got %s",
+							args[0].Type())
+					}
+					switch arg := args[1].(type) {
+					case *object.String:
+						splitter = arg.Value
+					default:
+						return newError("arguments to must be STRING, got %s",
+							args[0].Type())
+					}
+					var returnString = strings.Split(stringToSplit, splitter)
+					print(returnString)
+					//newElements := make([]object.Object, 2, 2)
+					//newElements[0] = returnString[0]
+					//newElements[1] = returnString[1]
+					return NULL //&object.Array{Elements: newElements}
+					//copy(newElements, returnString[0:len(returnString)])
+					//sort.Sort(sort.StringSlice(newElements[0:length]))
+					//fmt.Println(object.Array{newElements})
+					//WORK IN PROGRESS
+					//return &object.Array{Elements: newElements}
+				}
+
+			}
+			return NULL
 		},
 	},
 	"exit": &object.Builtin{
